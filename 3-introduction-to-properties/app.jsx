2837a4bba2ef57a7bf3e14f46c2fb37f51bcbfc2
Andy Zhang <azhang6@usnymazhang02.infor.com>
2015-09-16 15:35:47 -0400

    proper formatting for tutorials 1-4

--- 3-introduction-to-properties/app.jsx
+++ 3-introduction-to-properties/app.jsx
@@ -1,34 +1,32 @@
 var App = React.createClass({
-  getDefaultProps:function(){
-    return {
-      txt: 'this is a default prop',
-      cat: 0
-    }
-  },
-  propTypes: {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-  },
-  getInitialState:function(){
-    return {
-      txt: 'the inital state',
-      id: 0
-    }
-  },
-  update: function(e) {
-    this.setState({txt: e.target.value});
-  },
-  render:function(){
-
+    getDefaultProps:function(){
+        return {
+            txt: 'this is a default prop',
+            cat: 0
+        }
+    },
+    propTypes: {
+        txt: React.PropTypes.string,
+        cat: React.PropTypes.number.isRequired
+    },
+    getInitialState:function(){
+        return {
+            txt: 'the inital state',
+            id: 0
+        }
+    },
+    update: function(e) {
+        this.setState({txt: e.target.value});
+    },
+    render:function(){
     var txt = this.state.txt
-    return (
-      <div>
-        <input type="text" onChange={this.update} />
-        <h1>{txt}</h1>
-      </div>
-      );
-
-  }
+        return (
+            <div>
+                <input type="text" onChange={this.update} />
+                <h1>{txt}</h1>
+            </div>
+        );
+    }
 });
 
 React.render(<App cat={5} />, document.getElementById('myContainer'));
