1c6b2d686a8847ac6294dc8b63faff6f21d5489d
Andy Zhang <azhang6@usnymazhang02.infor.com>
2015-09-16 09:43:58 -0400

    update to run outside js bin rm folders from other branches References #6

--- 2-render-method/app.jsx
+++ 2-render-method/app.jsx
@@ -21,4 +21,4 @@ var App = React.createClass({
   }
 });
 
-ReactDOM.render(<App cat={5} />, document.getElementById('myContainer'));
+React.render(<App cat={5} />, document.getElementById('myContainer'));
